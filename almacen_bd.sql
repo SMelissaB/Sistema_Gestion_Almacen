6f12e1fee4e2a2e000b77c504167101071135e33
Sol Melissa <melissabolivarp@gmail.com>
2026-03-27 13:57:18 -0500

    Se agregó el script de la base de datos

---
+++ almacen_bd.sql
@@ -0,0 +1,75 @@
+CREATE DATABASE almacen_db;
+USE almacen_db;
+
+CREATE TABLE usuario (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE unidad_medida (
+    id_unidad INT AUTO_INCREMENT PRIMARY KEY,
+    codigo VARCHAR(10) NOT NULL UNIQUE,
+    descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE categoria (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    codigo VARCHAR(10) NOT NULL UNIQUE,
+    nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE proveedor (
+    id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
+    codigo VARCHAR(10) NOT NULL UNIQUE,
+    nombre VARCHAR(100) NOT NULL
+);
+
+-- 2. Tablas dependientes
+CREATE TABLE producto (
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
+    codigo VARCHAR(10) NOT NULL UNIQUE,
+    nombre VARCHAR(100) NOT NULL,
+    id_unidad INT,
+    precio_unitario DECIMAL(10,2),
+    id_categoria INT,
+    id_proveedor INT,
+    stock INT DEFAULT 0,
+    FOREIGN KEY (id_unidad) REFERENCES unidad_medida(id_unidad),
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
+    FOREIGN KEY (id_proveedor) REFERENCES proveedor(id_proveedor)
+);
+
+CREATE TABLE movimiento (
+    id_movimiento INT AUTO_INCREMENT PRIMARY KEY,
+    fecha DATE NOT NULL,
+    tipo_movimiento ENUM('ENTRADA', 'SALIDA') NOT NULL,
+    numero_documento VARCHAR(20),
+    id_usuario INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE detalle_movimiento (
+    id_detalle INT AUTO_INCREMENT PRIMARY KEY,
+    id_movimiento INT NOT NULL,
+    id_producto INT NOT NULL,
+    cantidad INT NOT NULL,
+    precio_historico DECIMAL(10,2),
+    saldo_momento INT,
+    FOREIGN KEY (id_movimiento) REFERENCES movimiento(id_movimiento),
+    FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
+);
+
+
+-- inserciones
+
+INSERT INTO usuario (username, password) 
+VALUES ('sise', '12345');
+
+INSERT INTO proveedor (codigo, nombre)
+VALUES
+('P001', 'Distribuidora Global'),
+('P002', 'Logística Express'),
+('P003', 'Soluciones Tecnológicas S.A.'),
+('P004', 'Suministros Industriales'),
+('P005', 'Comercializadora del Norte');
